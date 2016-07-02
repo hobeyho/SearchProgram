44331d9b5562a1de58d5fa02a3b7edc08802fe3f
Nick JongWook Park <nick.jw.park@gmail.com>
2016-07-02 15:09:08 +0900

    Added Search History

--- app/src/main/java/com/nickjwpark/searchprogram/MainActivity.java
+++ app/src/main/java/com/nickjwpark/searchprogram/MainActivity.java
@@ -1,17 +1,27 @@
 package com.nickjwpark.searchprogram;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
 
     EditText editTextSearch;
     Button btnSearch;
+    ArrayList<String> search_list;
+    SharedPreferences sharedPref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +31,17 @@ public class MainActivity extends AppCompatActivity {
         editTextSearch = (EditText) findViewById(R.id.editTextSearch);
         btnSearch = (Button) findViewById(R.id.btnSearch);
 
+        sharedPref = this.getSharedPreferences("com.nickjwpark.searchprogram", Context.MODE_PRIVATE);
+        search_list = new ArrayList<String>();
+        loadArrayList();
+        populateListview();
+
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String search = editTextSearch.getText().toString();
+                search_list.add(search);
+                saveArrayList();
                 searchStackOverflow(search);
             }
         });
@@ -40,4 +57,37 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("url", url);
         startActivity(intent);
     }
+
+    public void populateListview(){
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, search_list);
+        setListAdapter(adapter);
+    }
+
+    public void saveArrayList(){
+        String search_str = "";
+        for(String search : search_list){
+            search_str = search_str + search + "&&&";
+        }
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("search",search_str);
+        editor.commit();
+        populateListview();
+    }
+
+    public void loadArrayList(){
+        String defaultValue = "";
+        String todo = sharedPref.getString("search", defaultValue);
+        String [] search_arr = todo.split("&&&");
+        for(String search : search_arr){
+            search_list.add(search);
+        }
+    }
+
+    protected void onListItemClick(ListView l, View v, final int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        String search = search_list.get(position);
+        searchStackOverflow(search);
+    }
+
 }
